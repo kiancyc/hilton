64d599e8000ea353b9486f4e3df29658f11aac73
Martin <martin_cai@qq.com>
2023-10-09 17:25:50 +0800

    add supabase

--- index.ts
+++ index.ts
@@ -1,11 +1,46 @@
-// Import express and instantiate it
-import express from "npm:express@4"
-const app = express()
+import { load } from "https://deno.land/std@0.201.0/dotenv/mod.ts"
+import express, { Request, Response } from "npm:express@4"
+import { createClient } from 'npm:@supabase/supabase-js'
+import fallback from "npm:express-history-api-fallback"
 
-// Register a route
-app.get("/", (req: any, res: any) => {
-    res.send("Yes")
+const app = express()
+const env = await load()
+
+app.get("/ping", (req: Request, res: Response) => {
+    res.send("pong")
 })
 
-// Run the server!
+const supabase = createClient(env['SUPABASE_URL'], env['SUPABASE_PROJECT_KEY'], { auth: { persistSession: false } })
+
+app.get("/api/bot/:id", async (req: Request, res: Response) => {
+    const bot_id = req.params.id;
+    const { data, error } = await supabase
+        .from('bot')
+        .select()
+        .eq('id', bot_id)
+        .limit(1).single()
+
+    res.send(data)
+})
+
+app.get("/api/bot-list", async (req: Request, res: Response) => {
+    const { data, error } = await supabase
+        .from('bot')
+        .select().limit(10)
+
+    res.send(data)
+})
+
+app.get("/api/bot-create", async (req: Request, res: Response) => {
+    const { error } = await supabase
+        .from('bot')
+        .insert({
+            name: '人造人2号',
+        })
+    res.send(error || 'ok')
+})
+
+app.use(express.static('public'))
+app.use(fallback('index.html', { root: 'public' }))
+
 app.listen(80)
---
+++ public/block-header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="/bot">Bot</b-nav-item>
+                    <b-nav-item href="/bot-list">BotList</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: () => {
+            return {
+
+            }
+        },
+        methods: {},
+        mounted() {
+
+        }
+    }
+</script>
